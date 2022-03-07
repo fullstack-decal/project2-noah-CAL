77f33d006df10a7a80c54830025e0be6100d8f65
Noah Sedlik <noahsedlik@berkeley.edu>
2022-03-07 13:41:00 -0800

    add backspace functionality

--- calculator.js
+++ calculator.js
@@ -1,15 +1,26 @@
-//Check out calculator_hints.js for hints if you're stuck
 const calc = {
     numpad: document.querySelectorAll(".numpad"),
+    clearButton: document.querySelector("#c-button"),
+    backButton: document.querySelector("#back-button"),
     display: document.querySelector(".result-screen"),
-    resetToZero: function() {
+    resetDisplay: function() {
         this.display.innerHTML = "0";
     },
-    updateDisplay: function(numString) {
+    addNumber: function(numString) {
         if (this.display.innerHTML === "0") {
             this.display.innerHTML = numString;
         } else {
             this.display.innerHTML += numString;
+        }
+    },
+    removeNumber: function() {
+        if (this.display.innerHTML !== "0") {
+            console.log('h')
+            if (this.display.innerHTML.length > 1) {
+                this.display.innerHTML = this.display.innerHTML.slice(0, this.display.innerHTML.length - 1)
+            } else {
+                this.resetDisplay();
+            }
         }
     }
 }
@@ -19,6 +30,16 @@ calc.numpad.forEach(button =>  {
     button.addEventListener("click", e => {
         let number = button.innerHTML;
         console.log(number);
-        calc.updateDisplay(number)
+        calc.addNumber(number)
     })}
 )
+
+/* Clear keyboard on C press. */
+calc.clearButton.addEventListener("click", () => {
+    calc.resetDisplay();
+})
+
+/* Remove number from display on <- press. */
+calc.backButton.addEventListener("click", () => {
+    calc.removeNumber()
+})
