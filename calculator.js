c8ccb20a2f24e09411f4df2a02b5948da632e016
Noah Sedlik <noahsedlik@berkeley.edu>
2022-03-07 15:33:33 -0800

    add most calculator funcationality

--- calculator.js
+++ calculator.js
@@ -9,13 +9,13 @@ const calc = {
     calculate: {
         currNumber: 0,
         add: (num) => {
-            calc.calculate.currNumber + num
+            calc.calculate.currNumber += num
         },
         subtract: (num) => {
             calc.calculate.currNumber = Math.min(0, calc.calculate.currNumber - num)
         },
         multiply: (num) => {
-            calc.calculate.currNumber = calc.calculate.currNumber * num
+            calc.calculate.currNumber *= num
         },
         divide: (num) => {
             if (num === 0) {
@@ -27,11 +27,12 @@ const calc = {
         resetNumber: () => this.currNumber = 0,
     },
     display: {
-        displayText: () => document.querySelector(".result-screen").innerHTML,
+        getDisplayText: () => document.querySelector(".result-screen").innerHTML,
         updateDisplay: (numString) => calc.buttons.display.innerHTML = numString,
+        getNumber: () => parseInt(calc.display.getDisplayText()),
         resetDisplay: () => calc.display.updateDisplay(0),
         addNumber: function(numString) {
-            const displayText = this.displayText();
+            const displayText = this.getDisplayText();
             if (displayText === "0") {
                 this.updateDisplay(numString);
             } else {
@@ -39,7 +40,7 @@ const calc = {
             }
         },
         removeNumber: function() {
-            const displayText = this.displayText();
+            const displayText = this.getDisplayText();
             if (displayText !== "0") {
                 if (displayText.length <= 1) {
                     this.resetDisplay();
@@ -62,6 +63,7 @@ calc.buttons.numpad.forEach(button =>  {
 /* Clear keyboard on C press. */
 calc.buttons.clearButton.addEventListener("click", () => {
     calc.display.resetDisplay();
+    calc.calculate.resetNumber();
 })
 
 /* Remove number from display on <- press. */
@@ -69,7 +71,39 @@ calc.buttons.backButton.addEventListener("click", () => {
     calc.display.removeNumber()
 })
 
+/* TODO - REMOVE ME */
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        console.log(calc.calculate.currNumber)
+    })
+})
+
 calc.buttons.calcButtons.forEach(button => {
     button.addEventListener("click", () => {
+        const currNum = calc.display.getNumber()
+        switch (button.innerHTML) {
+            case '+':
+                calc.calculate.add(currNum)
+                break
+            case '-':
+                calc.calculate.subtract(currNum)
+                console.log('h')
+                break
+            case 'x':
+                calc.calculate.multiply(currNum)
+                break
+            case '÷':
+                calc.calculate.divide(currNum)
+                break
+            case '=':
+                //TODO FIXME
+                break
+        }
+        if (button.innerHTML === '=') {
+            calc.display.updateDisplay(calc.calculate.currNumber)
+            calc.calculate.resetNumber();
+        } else {
+            calc.display.updateDisplay(0)
+        }
     })
 })
