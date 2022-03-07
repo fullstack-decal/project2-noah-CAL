cb0e28bad9b4c31baf98465117782a8f5a2d9746
Noah Sedlik <noahsedlik@berkeley.edu>
2022-03-07 13:27:44 -0800

    add numpad display functionality (it correctly displays typed-out numbers

--- calculator.js
+++ calculator.js
@@ -1,11 +1,24 @@
 //Check out calculator_hints.js for hints if you're stuck
 const calc = {
     numpad: document.querySelectorAll(".numpad"),
+    display: document.querySelector(".result-screen"),
+    resetToZero: function() {
+        this.display.innerHTML = "0";
+    },
+    updateDisplay: function(numString) {
+        if (this.display.innerHTML === "0") {
+            this.display.innerHTML = numString;
+        } else {
+            this.display.innerHTML += numString;
+        }
+    }
 }
 
 /* Add click event listeners for each button. */
 calc.numpad.forEach(button =>  {
     button.addEventListener("click", e => {
-        console.log(button.innerHTML);
+        let number = button.innerHTML;
+        console.log(number);
+        calc.updateDisplay(number)
     })}
 )
