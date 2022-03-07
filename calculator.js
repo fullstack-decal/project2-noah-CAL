8cdc9a4aca59013588c3260f1e18493d17d2939f
Noah Sedlik <noahsedlik@berkeley.edu>
2022-03-07 14:44:52 -0800

    finish calculations functionality

--- calculator.js
+++ calculator.js
@@ -17,44 +17,47 @@ const calc = {
                 this.currNumber = Math.floor(this.currNumber / num)
             }
         },
+        resetNumber: () => this.currNumber = 0,
     },
-    displayText: () => document.querySelector(".result-screen").innerHTML,
-    updateDisplay: (numString) => calc.buttons.display.innerHTML = numString,
-    resetDisplay: () => calc.updateDisplay(0),
-    addNumber: function(numString) {
-        const displayText = this.displayText();
-        if (displayText === "0") {
-            this.updateDisplay(numString);
-        } else {
-            this.updateDisplay(displayText + numString);
-        }
-    },
-    removeNumber: function() {
-        const displayText = this.displayText();
-        if (displayText !== "0") {
-            if (displayText.length <= 1) {
-                this.resetDisplay();
+    display: {
+        displayText: () => document.querySelector(".result-screen").innerHTML,
+        updateDisplay: (numString) => calc.buttons.display.innerHTML = numString,
+        resetDisplay: () => calc.display.updateDisplay(0),
+        addNumber: function(numString) {
+            const displayText = this.displayText();
+            if (displayText === "0") {
+                this.updateDisplay(numString);
             } else {
-                this.updateDisplay(displayText.slice(0, displayText.length - 1))
+                this.updateDisplay(displayText + numString);
+            }
+        },
+        removeNumber: function() {
+            const displayText = this.displayText();
+            if (displayText !== "0") {
+                if (displayText.length <= 1) {
+                    this.resetDisplay();
+                } else {
+                    this.updateDisplay(displayText.slice(0, displayText.length - 1))
+                }
             }
         }
-    }
+    },
 }
 
 /* Add click event listeners for each button. */
 calc.buttons.numpad.forEach(button =>  {
     let number = button.innerHTML;
     button.addEventListener("click", e => {
-        calc.addNumber(number)
+        calc.display.addNumber(number)
     })}
 )
 
 /* Clear keyboard on C press. */
 calc.buttons.clearButton.addEventListener("click", () => {
-    calc.resetDisplay();
+    calc.display.resetDisplay();
 })
 
 /* Remove number from display on <- press. */
 calc.buttons.backButton.addEventListener("click", () => {
-    calc.removeNumber()
+    calc.display.removeNumber()
 })
