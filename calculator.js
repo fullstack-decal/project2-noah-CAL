8d8efe41555c2433ddac86704b282180780d4073
Noah Sedlik <noahsedlik@berkeley.edu>
2022-03-07 13:15:50 -0800

    add event listeners for number buttons

--- calculator.js
+++ calculator.js
@@ -1,1 +1,11 @@
 //Check out calculator_hints.js for hints if you're stuck
+const calc = {
+    numpad: document.querySelectorAll(".numpad"),
+}
+
+/* Add click event listeners for each button. */
+calc.numpad.forEach(button =>  {
+    button.addEventListener("click", e => {
+        console.log(button.innerHTML);
+    })}
+)
